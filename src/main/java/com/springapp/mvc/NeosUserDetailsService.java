c2e31925507f1041d1a82a63c8ee30a219975623
metairie <metairie@ebu.ch>
2015-09-02 16:52:35 +0200

    implement complete security. user+role

--- src/main/java/com/springapp/mvc/NeosUserDetailsService.java
+++ src/main/java/com/springapp/mvc/NeosUserDetailsService.java
@@ -1,9 +1,14 @@
 package com.springapp.mvc;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by metairie on 01-Sep-15.
@@ -13,6 +18,81 @@ public class NeosUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+        System.out.println(" user detail service for " + s);
+        Account account = new Account(s);
+        UserDetails user = new UserDetailsAdapter(account);
+        return user;
+    }
+
+    public class UserDetailsAdapter implements UserDetails {
+
+        Account account;
+
+        public UserDetailsAdapter(Account account) {
+            this.account = account;
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+            authorities.add(new GrantedAuthorityImpl(account));
+            return authorities;
+        }
+
+        @Override
+        public String getPassword() {
+            return account.password;
+        }
+
+        @Override
+        public String getUsername() {
+            return account.username;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+    }
+
+    public class GrantedAuthorityImpl implements GrantedAuthority {
+        Account account;
+
+        GrantedAuthorityImpl(Account account) {
+            this.account = account;
+        }
+
+        @Override
+        public String getAuthority() {
+            if (account.username.equalsIgnoreCase("root")) {
+                return "ROLE_ADMIN";
+            } else {
+                return "ROLE_PUBLIC";
+            }
+        }
+    }
+
+    private class Account {
+        String username;
+        String password = "";
+
+        public Account(String s) {
+            username = s;
+        }
     }
 }
--- src/main/java/com/springapp/mvc/SecurityConfig.java
+++ src/main/java/com/springapp/mvc/SecurityConfig.java
@@ -2,6 +2,10 @@ package com.springapp.mvc;
 
 /**
  * Created by metairie on 02-Sep-15.
+ *
+ * // TODO apply role to Controller directly
+ * //  is it possible .antMatchers("/admin/**").access("hasRole('ADMIN')")
+ * 
  */
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,7 +44,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("root").password("").roles("ADMIN");
-        auth.inMemoryAuthentication().withUser("agent").password("").roles("AGENT");
+//      OK works
+        // return the role according user account
+        // to be fully complete
+        // implement class Account (real world)
+        // this class could retrieve JDBC data
+
+        auth.userDetailsService(new NeosUserDetailsService());
+//      OK works
+//        auth.inMemoryAuthentication().withUser("root").password("").roles("ADMIN");
+//        auth.inMemoryAuthentication().withUser("agent").password("").roles("AGENT");
     }
 }
